8c1d148c49df49fd5809f5dbbbc014c54469fa49
Jeffrey Ouma <JWOuma@student.fullsail.edu>
2021-02-02 16:40:57 -0500

    02/02/21 Push

---
+++ DungeoneersSandboxDouble/Assets/Scripts/Wall.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public enum TileScetion
+	{
+        Center,
+        Edge,
+        Corner,
+        InnerCorner
+	}
+
+    [SerializeField]
+    GameObject[] m_slices = new GameObject[4];
+
+    //This function is called when the script is loaded or a value is changed in the inspector (Called in the editor only)
+    private void OnValidate()
+    {
+        //Make sure array size for m_handles can not be changed in editor
+        if (m_slices.Length != 5)
+        {
+            Array.Resize(ref m_slices, 5);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DungeoneersSandboxDouble/Assets/Scripts/WallTool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTool : MonoBehaviour
+{
+    Wall m_currentWall = null;
+    [SerializeField]
+    GameObject[] m_handles = new GameObject[5];
+
+    //This function is called when the script is loaded or a value is changed in the inspector (Called in the editor only)
+    private void OnValidate()
+	{
+        //Make sure array size for m_handles can not be changed in editor
+        if (m_handles.Length != 5)
+        {
+            Array.Resize(ref m_handles, 5);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
