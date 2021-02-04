6904fc11fec98f84dad6e5bbf3b4aa9b0637840b
Jeffrey Ouma <JWOuma@student.fullsail.edu>
2021-02-04 16:29:35 -0500

    04/02/21 Push

--- DungeoneersSandboxDouble/Assets/Scripts/Wall.cs
+++ DungeoneersSandboxDouble/Assets/Scripts/Wall.cs
@@ -13,26 +13,50 @@ public class Wall : MonoBehaviour
         InnerCorner
 	}
 
-    GameObject m_wall;
+    public class Slice
+	{
+        public GameObject slice;
+        public int cornerNeighbors = 0;
+        public Slice[] sliceNeighbors = new Slice[] { null,null,null,null,null,null,null,null };
+        public Slice Top { get { return sliceNeighbors[0]; } set { sliceNeighbors[0] = value; } }
+        public Slice TopRight { get { return sliceNeighbors[1]; } set { sliceNeighbors[1] = value; } }
+        public Slice Right { get { return sliceNeighbors[2]; } set { sliceNeighbors[2] = value; } }
+        public Slice BottomRight { get { return sliceNeighbors[3]; } set { sliceNeighbors[3] = value; } }
+        public Slice Bottom { get { return sliceNeighbors[4]; } set { sliceNeighbors[4] = value; } }
+        public Slice BottomLeft { get { return sliceNeighbors[5]; } set { sliceNeighbors[5] = value; } }
+        public Slice Left { get { return sliceNeighbors[6]; } set { sliceNeighbors[6] = value; } }
+        public Slice TopLeft { get { return sliceNeighbors[7]; } set { sliceNeighbors[7] = value; } }
+	}
+
+    //GameObject m_wall;
+    Slice m_originCenter = new Slice();
 
     [SerializeField]
     GameObject[] m_slices = new GameObject[4];
+    [SerializeField]
+    Vector3 m_positionOffset;
+    [SerializeField]
+    Vector3 m_rotationOffset;
+    [SerializeField]
+    Vector3 m_scaleOffset;
+    
+    public Slice OriginCenter { get { return m_originCenter; } }
 
 
     //This function is called when the script is loaded or a value is changed in the inspector (Called in the editor only)
     private void OnValidate()
     {
         //Make sure array size for m_handles can not be changed in editor
-        if (m_slices.Length != 5)
+        if (m_slices.Length != 4)
         {
-            Array.Resize(ref m_slices, 5);
+            Array.Resize(ref m_slices, 4);
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       GenerateOrigin();
     }
 
     // Update is called once per frame
@@ -43,6 +67,76 @@ public class Wall : MonoBehaviour
 
     public void GenerateOrigin()
 	{
+        m_originCenter.sliceNeighbors[0] = new Slice();
+        m_originCenter.sliceNeighbors[1] = new Slice();
+        m_originCenter.sliceNeighbors[2] = new Slice();
+        m_originCenter.sliceNeighbors[3] = new Slice();
+        m_originCenter.sliceNeighbors[4] = new Slice();
+        m_originCenter.sliceNeighbors[5] = new Slice();
+        m_originCenter.sliceNeighbors[6] = new Slice();
+        m_originCenter.sliceNeighbors[7] = new Slice();
 
+        m_originCenter.slice = Instantiate(m_slices[(int)TileScetion.Center], this.transform);
+        m_originCenter.TopLeft.slice = Instantiate(m_slices[(int)TileScetion.Corner], this.transform);
+        m_originCenter.TopRight.slice = Instantiate(m_slices[(int)TileScetion.Corner], this.transform);
+        m_originCenter.TopRight.slice.transform.Rotate(0.0f, 90.0f, 0.0f, Space.World);
+        m_originCenter.BottomRight.slice = Instantiate(m_slices[(int)TileScetion.Corner], this.transform); 
+        m_originCenter.BottomRight.slice.transform.Rotate(0.0f, 180.0f, 0.0f, Space.World);
+        m_originCenter.BottomLeft.slice = Instantiate(m_slices[(int)TileScetion.Corner], this.transform);
+        m_originCenter.BottomLeft.slice.transform.Rotate(0.0f, 270.0f, 0.0f, Space.World);
+        m_originCenter.Top.slice = Instantiate(m_slices[(int)TileScetion.Edge], this.transform);
+        m_originCenter.Right.slice = Instantiate(m_slices[(int)TileScetion.Edge], this.transform);
+        m_originCenter.Right.slice.transform.Rotate(0.0f, 90.0f, 0.0f, Space.World);
+        m_originCenter.Bottom.slice  = Instantiate(m_slices[(int)TileScetion.Edge], this.transform);
+        m_originCenter.Bottom.slice.transform.Rotate(0.0f, 180.0f, 0.0f, Space.World);
+        m_originCenter.Left.slice = Instantiate(m_slices[(int)TileScetion.Edge], this.transform);
+        m_originCenter.Left.slice.transform.Rotate(0.0f, 270.0f, 0.0f, Space.World);
+
+        m_originCenter.TopLeft.Right = m_originCenter.Top;
+        m_originCenter.TopLeft.BottomRight = m_originCenter;
+        m_originCenter.TopLeft.Bottom = m_originCenter.Left;
+
+        m_originCenter.Top.Left = m_originCenter.TopLeft;
+        m_originCenter.Top.BottomLeft = m_originCenter.Left;
+        m_originCenter.Top.Bottom = m_originCenter;
+        m_originCenter.Top.BottomRight = m_originCenter.Right;
+        m_originCenter.Top.Right = m_originCenter.TopRight;
+
+        m_originCenter.TopRight.Left = m_originCenter.Top;
+        m_originCenter.TopRight.BottomLeft = m_originCenter;
+        m_originCenter.TopRight.Bottom = m_originCenter.Right;
+
+        m_originCenter.Right.TopLeft = m_originCenter.Top;
+        m_originCenter.Right.Top = m_originCenter.TopRight;
+        m_originCenter.Right.Left = m_originCenter;
+        m_originCenter.Right.BottomLeft = m_originCenter.Bottom;
+        m_originCenter.Right.Bottom = m_originCenter.BottomRight;
+
+        m_originCenter.BottomRight.Top = m_originCenter.Right;
+        m_originCenter.BottomRight.TopLeft = m_originCenter;
+        m_originCenter.BottomRight.Left = m_originCenter.Bottom;
+
+        m_originCenter.Bottom.Left = m_originCenter.BottomLeft;
+        m_originCenter.Bottom.TopLeft = m_originCenter.Left;
+        m_originCenter.Bottom.Top = m_originCenter;
+        m_originCenter.Bottom.TopRight = m_originCenter.Right;
+        m_originCenter.Bottom.Right = m_originCenter.BottomRight;
+
+        m_originCenter.BottomLeft.Top = m_originCenter.Left;
+        m_originCenter.BottomLeft.TopRight = m_originCenter;
+        m_originCenter.BottomLeft.Right = m_originCenter.Bottom;
+
+        m_originCenter.Left.Top = m_originCenter.TopLeft;
+        m_originCenter.Left.TopRight = m_originCenter.Top;
+        m_originCenter.Left.Right = m_originCenter;
+        m_originCenter.Left.BottomRight = m_originCenter.Bottom;
+        m_originCenter.Left.Bottom = m_originCenter.BottomLeft;
+
+        for (int i = 0; i < transform.childCount; i++)
+		{
+            transform.GetChild(i).transform.localPosition = new Vector3(m_positionOffset.x, m_positionOffset.y, m_positionOffset.z);
+            transform.GetChild(i).transform.localScale = new Vector3(m_scaleOffset.x, m_scaleOffset.y, m_scaleOffset.z);
+            transform.GetChild(i).transform.Rotate(m_rotationOffset.x,m_rotationOffset.y,m_rotationOffset.z, Space.Self);
+		}
 	}
 }
--- DungeoneersSandboxDouble/Assets/Scripts/WallTool.cs
+++ DungeoneersSandboxDouble/Assets/Scripts/WallTool.cs
@@ -19,6 +19,7 @@ public class WallTool : MonoBehaviour
     Transform m_currentPivot = null;
     Transform m_currentHandle = null;
     handle_pivots[] pivots = new handle_pivots[] { handle_pivots.North, handle_pivots.East, handle_pivots.South, handle_pivots.West };
+    float dot;
     [SerializeField]
     GameObject[] m_handles = new GameObject[5];
 
@@ -52,48 +53,87 @@ public class WallTool : MonoBehaviour
                 if (index > 0) {
                     m_currentPivot = m_handles[(int)pivots[index - 1]].transform;
                 }
+                else
+				{
+                    m_currentPivot = null;
+				}
+                dot = Vector3.Dot(m_currentHandle.position, Camera.main.transform.forward);
             }
             else if(hit.transform.gameObject.TryGetComponent<Wall>(out m_currentWall))
 			{
                 Array.ForEach(m_handles, h => h.SetActive(true));
 			}
-		}
+        }
 
         if(Input.GetMouseButton(0) && m_currentHandle != null && m_currentPivot != null && m_currentWall != null)
 		{
             //TODO: Add handle movement
-            float dot = Vector3.Dot(m_currentHandle.forward, Camera.main.transform.forward);
             dot /= Mathf.Abs(dot);
-            if (Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse Y") > 0)
-			{
-                m_currentHandle.position += m_currentHandle.forward * dot * 0.05f;
-
+            if (Input.GetAxis("Mouse X") > 0 /*|| Input.GetAxis("Mouse Y") > 0*/)
+            {
+                if (m_currentHandle.gameObject == m_handles[1] && (m_currentHandle.position + m_currentHandle.up * -dot * 0.05f).z > 0.7f ||
+                    m_currentHandle.gameObject == m_handles[2] && (m_currentHandle.position + m_currentHandle.up * -dot * 0.05f).x > 0.7f ||
+                    m_currentHandle.gameObject == m_handles[3] && (m_currentHandle.position + m_currentHandle.up * -dot * 0.05f).z < -0.7f ||
+                    m_currentHandle.gameObject == m_handles[4] && (m_currentHandle.position + m_currentHandle.up * -dot * 0.05f).x < -0.7f)
+                {
+                    m_currentHandle.position += m_currentHandle.up * -dot * 0.05f;
+                }
             }
-            else if(Input.GetAxis("Mouse X") < 0 || Input.GetAxis("Mouse Y") < 0)
+            else if(Input.GetAxis("Mouse X") < 0 /*|| Input.GetAxis("Mouse Y") < 0*/)
 			{
-                m_currentHandle.position += m_currentHandle.forward * -dot * 0.05f;
+                if (m_currentHandle.gameObject == m_handles[1] && (m_currentHandle.position + m_currentHandle.up * dot * 0.05f).z > 0.7f ||
+                    m_currentHandle.gameObject == m_handles[2] && (m_currentHandle.position + m_currentHandle.up * dot * 0.05f).x > 0.7f ||
+                    m_currentHandle.gameObject == m_handles[3] && (m_currentHandle.position + m_currentHandle.up * dot * 0.05f).z < -0.7f ||
+                    m_currentHandle.gameObject == m_handles[4] && (m_currentHandle.position + m_currentHandle.up * dot * 0.05f).x < -0.7f)
+                {
+                    m_currentHandle.position += m_currentHandle.up * dot * 0.05f;
+                }
             }
 
 
 			if ((m_currentHandle.position.y == m_currentPivot.position.y) && (m_currentHandle.position.x == m_currentPivot.position.x) ||(m_currentHandle.position.z == m_currentPivot.position.z))
 			{
                 Vector3 midpooint;
-                float distance = Vector3.Distance(m_currentHandle.position, m_currentPivot.position) - 0.14f;
+                float distance = Vector3.Distance(m_currentHandle.position, m_currentPivot.position) - 0.7f;
+
                 if (m_currentHandle.position.x == m_currentPivot.position.x) {
-                    midpooint = ((m_currentHandle.position.z > m_currentPivot.position.z) ? m_currentHandle.position - m_currentPivot.position : m_currentPivot.position - m_currentHandle.position);
-                    midpooint /= 2.0f;
-                    m_currentWall.transform.position = midpooint;
                     m_currentWall.transform.localScale = new Vector3(m_currentWall.transform.localScale.x, m_currentWall.transform.localScale.y, distance);
                 }
 				else 
                 {
-                    midpooint = ((m_currentHandle.position.x > m_currentPivot.position.x) ? m_currentHandle.position - m_currentPivot.position : m_currentPivot.position - m_currentHandle.position);
-                    midpooint /= 2.0f;
-                    m_currentWall.transform.position = midpooint;
-                    m_currentWall.transform.localScale = new Vector3(m_currentWall.transform.localScale.x, m_currentWall.transform.localScale.y, distance);
+                    m_currentWall.transform.localScale = new Vector3(distance, m_currentWall.transform.localScale.y, m_currentWall.transform.localScale.z);
                 }
+
+                if(m_currentWall.transform.localScale.x < 1.0f)
+				{
+                    m_currentWall.transform.localScale = new Vector3(1.0f, m_currentWall.transform.localScale.y, m_currentWall.transform.localScale.z);
+
+                }
+                if(m_currentWall.transform.localScale.y < 1.0f)
+				{
+                    m_currentWall.transform.localScale = new Vector3(m_currentWall.transform.localScale.x, 1.0f, m_currentWall.transform.localScale.z);
+                }
+                if(m_currentWall.transform.localScale.z < 1.0f)
+				{
+                    m_currentWall.transform.localScale = new Vector3(m_currentWall.transform.localScale.x, m_currentWall.transform.localScale.y, 1.0f);
+                }
+
+                m_currentWall.transform.position = (m_currentHandle.position + m_currentPivot.position) / 2.0f;
+
+                if(m_currentHandle.gameObject == m_handles[1] || m_currentHandle.gameObject == m_handles[3])
+				{
+                    m_handles[2].transform.position = new Vector3(m_handles[2].transform.position.x, m_handles[2].transform.position.y, m_currentWall.transform.position.z);
+                    m_handles[4].transform.position = new Vector3(m_handles[4].transform.position.x, m_handles[4].transform.position.y, m_currentWall.transform.position.z);
+				}
+				else
+				{
+                    m_handles[1].transform.position = new Vector3(m_currentWall.transform.position.x, m_handles[1].transform.position.y, m_handles[1].transform.position.z);
+                    m_handles[3].transform.position = new Vector3(m_currentWall.transform.position.x, m_handles[3].transform.position.y, m_handles[3].transform.position.z);
+                }
+
+
             }
-		}
+        }
 
 
     }
