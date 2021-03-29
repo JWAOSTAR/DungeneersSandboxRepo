4ff9dafeeadb8b4929533e562c289586a29425be
Jeffrey Ouma <jwaostar@gmail.com>
2021-03-29 16:04:12 -0400

    29/03/21 Push

---
+++ DungeoneersSandboxDouble/Assets/Scripts/ProceduralObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewProceduralObject", menuName = "DungoneersSandbox/Procedural Object", order = 3)]
+public class ProceduralObject : ScriptableObject
+{
+	[SerializeField]
+	bool singleObject = false;
+	public GameObject[] objectOptions;
+	[SerializeField]
+	Material[] materialOptions;
+	[Space]
+	[SerializeField]
+	[Range(1, 9)]
+	int tileVolume = 1;
+	[Space]
+	[SerializeField]
+	bool varientHeight = false;
+	[SerializeField]
+	float height = 1.0f;
+	[Space]
+	[SerializeField]
+	bool singleColor = true;
+	public Color[] tileColors = new Color[1];
+	[Space]
+	public Texture2D tileTexture;
+	public Texture2D tileNormal;
+	public Texture2D tileAmbientOcclusion;
+
+	private void OnValidate()
+	{
+		if (singleColor)
+		{
+			Array.Resize(ref tileColors, 1);
+		}
+		else
+		{
+			Array.Resize(ref tileColors, 6);
+		}
+		if(height < 0.0f)
+		{
+			height = 1.0f;
+		}
+		if(singleObject)
+		{
+			Array.Resize(ref objectOptions, 1);
+			varientHeight = false;
+		}
+	}
+}
