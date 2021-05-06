eef03f6f6ba2e42970ecd3194c0ccbd4b7bddc52
Jeffrey Ouma <jwaostar@gmail.com>
2021-05-06 17:34:03 -0400

    06/04/21 Push

--- DungeoneersSandboxDouble/Assets/Scripts/Playground.cs
+++ DungeoneersSandboxDouble/Assets/Scripts/Playground.cs
@@ -2,17 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
 public class Playground : MonoBehaviourPunCallbacks
 {
+	[SerializeField]
+	GameObject LoadingScreen;
+	[SerializeField]
+	GameObject ConnectScreen;
+	[SerializeField]
+	GameObject RoomList;
+	[SerializeField]
+	GameObject RoomListItem;
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+		RoomList.SetActive(false);
+		RoomListItem.SetActive(false);
     }
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.N))
+		{
+			Instantiate(RoomListItem, RoomListItem.transform.parent);
+		}
+	}
+
+	public void CreateRoom()
+	{
+		if (!PhotonNetwork.IsConnected)
+		{
+			PhotonNetwork.GameVersion = Application.version;
+			PhotonNetwork.ConnectUsingSettings();
+			LoadingScreen.SetActive(true);
+		}
+		//TODO: Add dialog to ask for room name, have dialog cal CreateRoom(string)
+	}
+
+	public void CreateRoom(Text _roomName)
+	{
+		PhotonNetwork.CreateRoom(_roomName.text);
+	}
+
+	public void ConnectToRoom()
+	{
+		if (!PhotonNetwork.IsConnected)
+		{
+			PhotonNetwork.GameVersion = Application.version;
+			PhotonNetwork.ConnectUsingSettings();
+			LoadingScreen.SetActive(true);
+		}
+		//TODO: Add dialog to ask for room name, have dialog cal ConnectToRoom(string)
+	}
+
+	public void ConnectToRoom(string _room)
+	{
+		PhotonNetwork.JoinRoom(_room);
+	}
 
 	public override void OnConnectedToMaster()
 	{
+		Debug.Log("CONNECTED TO SERVER");
+		LoadingScreen.SetActive(false);
+		ConnectScreen.SetActive(true);
+		StartCoroutine("ConnectScreenTimeOut");
 		base.OnConnectedToMaster();
         PhotonNetwork.JoinLobby();
 	}
@@ -23,9 +79,44 @@ public class Playground : MonoBehaviourPunCallbacks
 		//TODO SET UP MAP AND TILES
 	}
 
-	// Update is called once per frame
-	void Update()
-    {
-        
-    }
+	public override void OnJoinedRoom()
+	{
+		base.OnJoinedRoom();
+	}
+
+	public override void OnCreatedRoom()
+	{
+		Debug.Log("ROOM CREATED");
+		base.OnCreatedRoom();
+	}
+
+	public override void OnRoomListUpdate(List<RoomInfo> roomList)
+	{
+		for(int i = RoomListItem.transform.parent.childCount - 1; i < roomList.Count; i++)
+		{
+			RoomList.SetActive(true);
+			if (i == 0)
+			{
+				RoomListItem.SetActive(true);
+				RoomListItem.transform.GetComponentInChildren<Text>().text = roomList[i].Name;
+			}
+			else
+			{
+				Instantiate(RoomListItem, RoomListItem.transform.parent).transform.GetComponentInChildren<Text>().text = roomList[i].Name;
+			}
+		}
+		base.OnRoomListUpdate(roomList);
+	}
+
+	public override void OnDisconnected(DisconnectCause cause)
+	{
+		Debug.Log("DISCONNECTED FROM SERVER: " + cause.ToString());
+		base.OnDisconnected(cause);
+	}
+
+	IEnumerator ConnectScreenTimeOut()
+	{
+		yield return new WaitForSeconds(0.8f);
+		ConnectScreen.SetActive(false);
+	}
 }
